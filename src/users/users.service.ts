54cbbe310139fb108fe1038b5080a34dff579e80
SoeSandarWin <soesandarwin2201@gmail.com>
2024-04-21 10:42:46 +0700

    Add the operation into userservice

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { FetchAllUsers } from './operations/getAllUsers';
+import { UserConnection } from './dto/user.dto';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+   constructor(private readonly fetchAllUsers: FetchAllUsers){}
+
+   async getAllUsers(
+      authId: string,
+      offset: number,
+      limit: number,
+   ): Promise<UserConnection>{
+      return this.fetchAllUsers.getAllUser(authId, offset, limit)
+   }
+}
