3666e009dd06cb1bb0e1954eb03e11f29ee47902
SoeSandarWin <soesandarwin2201@gmail.com>
2024-04-21 10:53:59 +0700

    Add the apiquery for format api endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+   constructor(private readonly usersService: UsersService){}
+
+   @Get()
+   @ApiQuery({ name: 'offset', required: false, type: Number})
+   async getAllUser() {
+
+   }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './models/users.model';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { FetchAllUsers } from './operations/getAllUsers';
 
 @Module({
    imports: [
       MongooseModule.forFeature([{ name: User.name, schema: UserSchema}])
    ],
-   providers: [UsersService],
+   providers: [UsersService, FetchAllUsers],
    controllers: [UsersController],
    
 })  
