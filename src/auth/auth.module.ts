d4ffe836e10531f09b53b19295b0e58574fab2e6
SoeSandarWin <soesandarwin2201@gmail.com>
2024-04-23 21:20:51 +0700

    Create the controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Register } from './operations/register.operation';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/users/models/users.model';
+import { AuthController } from './auth.controller';
+
 
 @Module({
-   imports: [],
-   providers: [AuthService],
-   controllers: []
+   imports: [
+      MongooseModule.forFeature([{ name: User.name, schema: UserSchema}])
+   ],
+   providers: [AuthService, Register],
+   controllers: [AuthController]
 })
 export class AuthModule {}
