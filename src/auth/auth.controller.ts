6eb767fbf2443370f5a5c7f446fb44cf724825ec
SoeSandarWin <soesandarwin2201@gmail.com>
2024-04-23 21:26:35 +0700

    add method to auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { RegisterInput } from './inputs/register.input';
+import { AuthResponse } from './dto/authResponse.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+   constructor(private readonly authService: AuthService){}
+
+   @Post('register')
+   async register(@Body() input: RegisterInput):Promise<AuthResponse>{
+      return true
+   }
+
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Register } from './operations/register.operation';
+import { AuthResponse } from './dto/authResponse.dto';
+import { RegisterInput } from './inputs/register.input';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+   constructor(readonly authRegister: Register){}
+
+   async register(input: RegisterInput):Promise<AuthResponse>{
+      return this.authRegister.register(input)
+   }
+}
