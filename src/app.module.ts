a37b5d35f544cba6f18b6d5c1987ea9357596760
SoeSandarWin <soesandarwin2201@gmail.com>
2024-04-23 22:20:31 +0700

    Genereate the shop module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nestJob'),
     UsersModule,
     AuthModule,
+    ShopModule,
   ],
   controllers: [],
   providers: [],
