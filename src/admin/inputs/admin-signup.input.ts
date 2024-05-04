9e2f31a7c92872e831d778e45a7a2f84fef20fcc
SoeSandarWin <soesandarwin2201@gmail.com>
2024-05-04 17:18:43 +0700

    Create the admin signup input

---
+++ src/admin/inputs/admin-signup.input.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty,IsPhoneNumber, IsEmail, Length, Validate} from "class-validator"
+import { PasswordValidator } from "src/utility/auth/password.validator"
+
+export class AdminSignUpInput{
+   @IsNotEmpty({ message: "Name is required"})
+   @Length(3,20,{ message: "Name must be between 6 to 20 characters"})
+   fullName: string
+
+   @IsNotEmpty({ message: "Email is required"})
+   @IsEmail(null, { message: "Invalid Email address"})
+   @Length(6,40,{ message: "Email must be between 6 to 40 characters"})
+   email: string
+
+   @IsNotEmpty({ message: "Password is required"})
+   @Validate(PasswordValidator, { message: "Password need to match with requirement"})
+   password: string
+}
