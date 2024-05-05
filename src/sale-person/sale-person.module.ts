e4f235f76e8741eb098fbca557c7c37d14ae7e7e
SoeSandarWin <soesandarwin2201@gmail.com>
2024-05-05 14:29:35 +0700

    Add the method to the service

--- src/sale-person/sale-person.module.ts
+++ src/sale-person/sale-person.module.ts
@@ -5,13 +5,14 @@ import { GetAllSalePerson } from './operation/getAllSalePerson.operation';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Admin, AdminSchema } from 'src/admin/models/admins.model';
 import { SalePerson, SalePersonSchema } from './models/saleperson.model';
+import { SalePersonSignIn } from './operation/saleperson-signin.operation';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema}]),
     MongooseModule.forFeature([{ name: SalePerson.name, schema: SalePersonSchema}]),
   ],
-  providers: [SalePersonService, GetAllSalePerson],
+  providers: [SalePersonService, GetAllSalePerson, SalePersonSignIn],
   controllers: [SalePersonController]
 })
 export class SalePersonModule {}
--- src/sale-person/sale-person.service.ts
+++ src/sale-person/sale-person.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { GetAllSalePerson } from './operation/getAllSalePerson.operation';
 import { SalePerson } from './models/saleperson.model';
+import { SalePersonSignIn } from './operation/saleperson-signin.operation';
+import { SuccessResponse } from 'src/utility/constant/Response.input';
+import { SalePersonSignInInput } from './input/saleperson-signIn.input';
 
 @Injectable()
 export class SalePersonService {
-   constructor(private readonly getAllSales: GetAllSalePerson){}
+   constructor(private readonly getAllSales: GetAllSalePerson,
+       private readonly saleSignIn: SalePersonSignIn
+   ){}
 
    async getAllSalePerson(adminId: string, offset: number, limit: number):Promise<SalePerson[]>{
       return await this.getAllSales.getAllSalePerson(adminId,offset,limit)
+   }
+
+   async salePersonSignIn(input: SalePersonSignInInput):Promise<SuccessResponse>{
+      return await this.saleSignIn.SalePersonSignIn(input)
+
    }
 }
